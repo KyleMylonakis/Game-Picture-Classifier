9820f6cdd244f0c78cf80496105426da47b9e5a3
Jay <jayroberts91@gmail.com>
2017-11-09 22:14:53 -0800

    Wrapped in a main()

--- ImagesProcessor.py
+++ ImagesProcessor.py
@@ -23,125 +23,129 @@ import CommunityImages
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Make the save directory
-if not os.path.isdir('processed/'):
-    os.makedirs('processed/')
 
-# Set the desired resolution
-resolution = (28,28)
+def main():
+    # Make the save directory
+    if not os.path.isdir('processed/'):
+        os.makedirs('processed/')
 
-# Check if the rawimage file exists
-if not(os.path.isdir('rawimages')):
-    print('rawimages file not found')
-    print('Running CommunityImages')
+    # Set the desired resolution
+    resolution = (28,28)
 
-    # USING DEFAULT NUM ARGUMENT [5,2,10]!
-    CommunityImages.main(os_in = False)
-    
-else:
-    print('Found rawimages file')
+    # Check if the rawimage file exists
+    if not(os.path.isdir('rawimages')):
+        print('rawimages file not found')
+        print('Running CommunityImages')
 
-# Get a list of the game folder names
-# Game Folders are named by AppID
-games_folders = os.listdir('rawimages/')
-n_fold = len(games_folders)
-
-# Get the game data
-g_df = pd.read_csv('Top100Games.csv')
-
-# Make the data and labels tensor
-all_processed = [0]*n_fold
-labels = [0]*n_fold
-
-# Columns for a data-frame organized by game
-appid_col  = [0]*n_fold
-images_col = [0]*n_fold
-
-# Loop through the games
-# Count total images
-total_images = 0
-for ifol in range(n_fold):
-    
-    g_name = games_folders[ifol]
-
-    # Get gameID
-    g_ix = ifol
-    g_ID = g_df['STEAM ID'].iloc[ifol]
-    
-    # Find image folder
-    g_folder = 'rawimages/'+g_name
-    g_image_folder = g_folder+'/downloads'
-
-    # Make folder to store the game's processed images for comparison sake
-    if not os.path.isdir(g_folder+'/processed'):
-        os.makedirs(g_folder+'/processed')
-
-    # Some files don't have images.
-    # Currently runs the WHOLE Community images. Change this to download only the missing games
-    # Maybe keep the number of games in the download folder in the Top100Games.csv  
-    #if not os.path.isdir(g_image_folder):
-    #    CommunityImages.main()
-
-    # Get the names of image files
-    g_images = os.listdir(g_image_folder)
-
-    # Set number of images
-    num_imgs = len(g_images)
-
-    # Create the tensor to hold the game's image arrays
-    g_proc_images = np.zeros((len(g_images), resolution[0], resolution[1],3))
-    
-    # Loop through the images
-    for ig in range(num_imgs):
-        # Get image and make a file path
-        image = g_images[ig]
-        img_path = g_image_folder+'/'+image
+        # USING DEFAULT NUM ARGUMENT [5,2,10]!
+        CommunityImages.main(os_in = False)
         
-        # Read picture
-        img_array = ndimage.imread(img_path)
+    else:
+        print('Found rawimages file')
 
-        #
-        # Here is where we can add any other preprocessing we like
-        #
+    # Get a list of the game folder names
+    # Game Folders are named by AppID
+    games_folders = os.listdir('rawimages/')
+    n_fold = len(games_folders)
 
-        # Change the resolution
-        img_array = misc.imresize(img_array, resolution)
+    # Get the game data
+    g_df = pd.read_csv('Top100Games.csv')
 
-        # Update g_proc_images
-        g_proc_images[ig] = img_array
+    # Make the data and labels tensor
+    all_processed = [0]*n_fold
+    labels = [0]*n_fold
 
-        # Save the processed image for future inspection
-        misc.imsave(g_folder+'/processed/%d_%d.jpg' % (g_ID,ig), img_array)
+    # Columns for a data-frame organized by game
+    appid_col  = [0]*n_fold
+    images_col = [0]*n_fold
 
-        # Count
-        total_images += 1
+    # Loop through the games
+    # Count total images
+    total_images = 0
+    for ifol in range(n_fold):
         
-    # Make it an array
-    g_proc_images = np.array(g_proc_images)
-    
-    
+        g_name = games_folders[ifol]
 
-    # Update all_processed 
-    all_processed[ifol] = g_proc_images
-    
-    # Update labels, appid and images
-    labels[ifol] = [g_ID]*num_imgs    
-    appid_col[ifol], images_col[ifol] = g_ID, g_proc_images
-
-# Make all_processed and labels into arrays
-all_processed = np.concatenate(all_processed, axis = 0)
-labels = np.concatenate(labels, axis = 0)
-
-# Unwind them
-all_processed.shape = (total_images, resolution[0],resolution[1],3)
-labels.shape = (total_images)
-
-# Save the data
-np.save('processed/'+'proc_imgs_tensor',all_processed)
-np.save('processed/'+'proc_labels_tensor', labels)
-
-
-
-
+        # Get gameID
+        g_ix = ifol
+        g_ID = g_df['STEAM ID'].iloc[ifol]
         
-    
+        # Find image folder
+        g_folder = 'rawimages/'+g_name
+        g_image_folder = g_folder+'/downloads'
+
+        # Make folder to store the game's processed images for comparison sake
+        if not os.path.isdir(g_folder+'/processed'):
+            os.makedirs(g_folder+'/processed')
+
+        # Some files don't have images.
+        # Currently runs the WHOLE Community images. Change this to download only the missing games
+        # Maybe keep the number of games in the download folder in the Top100Games.csv  
+        #if not os.path.isdir(g_image_folder):
+        #    CommunityImages.main()
+
+        # Get the names of image files
+        g_images = os.listdir(g_image_folder)
+
+        # Set number of images
+        num_imgs = len(g_images)
+
+        # Create the tensor to hold the game's image arrays
+        g_proc_images = np.zeros((len(g_images), resolution[0], resolution[1],3))
+        
+        # Loop through the images
+        for ig in range(num_imgs):
+            # Get image and make a file path
+            image = g_images[ig]
+            img_path = g_image_folder+'/'+image
+            
+            # Read picture
+            img_array = ndimage.imread(img_path)
+
+            #
+            # Here is where we can add any other preprocessing we like
+            #
+
+            # Change the resolution
+            img_array = misc.imresize(img_array, resolution)
+
+            # Update g_proc_images
+            g_proc_images[ig] = img_array
+
+            # Save the processed image for future inspection
+            misc.imsave(g_folder+'/processed/%d_%d.jpg' % (g_ID,ig), img_array)
+
+            # Count
+            total_images += 1
+            
+        # Make it an array
+        g_proc_images = np.array(g_proc_images)
+        
+        
+
+        # Update all_processed 
+        all_processed[ifol] = g_proc_images
+        
+        # Update labels, appid and images
+        labels[ifol] = [g_ID]*num_imgs    
+        appid_col[ifol], images_col[ifol] = g_ID, g_proc_images
+
+    # Make all_processed and labels into arrays
+    all_processed = np.concatenate(all_processed, axis = 0)
+    labels = np.concatenate(labels, axis = 0)
+
+    # Unwind them
+    all_processed.shape = (total_images, resolution[0],resolution[1],3)
+    labels.shape = (total_images)
+
+    # Save the data
+    np.save('processed/'+'proc_imgs_tensor',all_processed)
+    np.save('processed/'+'proc_labels_tensor', labels)
+
+if __name__ == '__main__':
+    main()
+
+
+
+            
+        
